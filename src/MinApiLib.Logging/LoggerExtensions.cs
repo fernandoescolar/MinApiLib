91810eafe5257b7fb95192ad8896378e1bb2a165
Fernando Escolar <fer.escolar@gmail.com>
2023-06-26 16:35:57 +0200

    updating logger format messages

--- src/MinApiLib.Logging/LoggerExtensions.cs
+++ src/MinApiLib.Logging/LoggerExtensions.cs
@@ -5,12 +5,12 @@ namespace MinApiLib.Logging;
 
 public static class LoggerExtensions
 {
-    private static readonly Action<ILogger, string, Exception?> _loggerMessageCritical = LoggerMessage.Define<string>(LogLevel.Critical, new EventId(100, nameof(LogLevel.Critical)), "{Message}");
-    private static readonly Action<ILogger, string, Exception?> _loggerMessageDebug = LoggerMessage.Define<string>(LogLevel.Debug, new EventId(200, nameof(LogLevel.Debug)), "{Message}");
-    private static readonly Action<ILogger, string, Exception?> _loggerMessageError = LoggerMessage.Define<string>(LogLevel.Error, new EventId(300, nameof(LogLevel.Error)), "{Message}");
-    private static readonly Action<ILogger, string, Exception?> _loggerMessageInformation = LoggerMessage.Define<string>(LogLevel.Information, new EventId(400, nameof(LogLevel.Information)), "{Message}");
-    private static readonly Action<ILogger, string, Exception?> _loggerMessageTrace = LoggerMessage.Define<string>(LogLevel.Trace, new EventId(500, nameof(LogLevel.Trace)), "{Message}");
-    private static readonly Action<ILogger, string, Exception?> _loggerMessageWarning = LoggerMessage.Define<string>(LogLevel.Warning, new EventId(600, nameof(LogLevel.Warning)), "{Message}");
+    private static readonly Action<ILogger, string, Exception?> _loggerMessageCritical = LoggerMessage.Define<string>(LogLevel.Critical, new EventId(100, nameof(LogLevel.Critical)), "{Critical}");
+    private static readonly Action<ILogger, string, Exception?> _loggerMessageDebug = LoggerMessage.Define<string>(LogLevel.Debug, new EventId(200, nameof(LogLevel.Debug)), "{Debug}");
+    private static readonly Action<ILogger, string, Exception?> _loggerMessageError = LoggerMessage.Define<string>(LogLevel.Error, new EventId(300, nameof(LogLevel.Error)), "{Error}");
+    private static readonly Action<ILogger, string, Exception?> _loggerMessageInformation = LoggerMessage.Define<string>(LogLevel.Information, new EventId(400, nameof(LogLevel.Information)), "{Information}");
+    private static readonly Action<ILogger, string, Exception?> _loggerMessageTrace = LoggerMessage.Define<string>(LogLevel.Trace, new EventId(500, nameof(LogLevel.Trace)), "{Trace}");
+    private static readonly Action<ILogger, string, Exception?> _loggerMessageWarning = LoggerMessage.Define<string>(LogLevel.Warning, new EventId(600, nameof(LogLevel.Warning)), "{Warning}");
 
     [MethodImpl(MethodImplOptions.NoInlining)]
     public static void LogFaster(this ILogger logger, LogLevel logLevel, string message, Exception? ex)
